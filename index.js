fc217574b6c57970f0a9382f6a339333782f3961
Cobrettie <cobrettiegarner3@gmail.com>
2020-06-04 19:42:16 -0400

    install shortid

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const server = express();
+const shortid = require('shortid');
 
 server.use(express.json());
 
@@ -10,27 +11,34 @@ server.get('/', (request, response) => {
   response.json({ message: 'hello im ur server' })
 })
 
+// Create = Post
 server.post('/api/users', (req, res) => {
   const userInfo = req.body;
+  userInfo.id = shortid.generate();
   users.push(userInfo);
-
-  res.status(201).json(hubInfo);
+  res.status(201).json(userInfo);
 })
 
+// Read = Get
 server.get('/api/users', (req, res) => {
   // return an array
-  // res.status().json()
+  res.status(200).json(users)
 })
 
 server.get('/api/users/:id', (req, res) => {
   // return user object that matches id
 })
 
+// Delete
 server.delete('/api/users/:id', (req, res) => {
   // remove user with specified id
   // returns deleted user
+
+  const { id } = req.params;
+  console.log(id);
 })
 
+// Edit
 server.put('/api/users/:id', (req, res) => {
   // edit user with same id
   // uses data from req body
@@ -46,5 +54,5 @@ server.put('/api/users/:id', (req, res) => {
 const PORT = 4000;
 
 server.listen(PORT, (req, res) => {
-  console.log(`server listening mate, check localhost ${PORT}`, req, res)
+  console.log(`server listening mate`)
 })
