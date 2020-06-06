8d140b28f39037902d745753dd2e425b2c758aec
Cobrettie <cobrettiegarner3@gmail.com>
2020-06-06 19:49:56 -0400

    install styled components

--- application/node-react-contactlist/src/components/ContactList/ContactCard.js
+++ application/node-react-contactlist/src/components/ContactList/ContactCard.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function ContactCard({ contact }) {
   const { id, name, bio} = contact;
   return (
     <div>
+      <p>{id}</p>
       <p>{name}</p>
       <p>{bio}</p>
     </div>
