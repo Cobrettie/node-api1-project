4c31230141825fd06c9cea739ece37bb012d58a8
Cobrettie <cobrettiegarner3@gmail.com>
2020-06-06 19:39:50 -0400

    server hosted, able to make HTTP requests + add biolerplate to ContactList component

--- application/node-react-contactlist/src/App.js
+++ application/node-react-contactlist/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ContactList from './components/ContactList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>React, Node Contact List</h1>
+      <ContactList />
     </div>
   );
 }
--- application/node-react-contactlist/src/components/ContactList.js
+++ application/node-react-contactlist/src/components/ContactList.js
@@ -1,11 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function ContactList() {
   const [contactList, setContactList] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:4000/api/users')
+      .then(response => {
+        console.log(response)
+        setContactList(response.data)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
   return (
     <div>
       <h2>Contact List</h2>
+      {contactList.map(contact => {
+        return (
+          <div key={contact.id}>
+            <p>{contact.name}</p>
+            <p>{contact.age}</p>
+          </div>
+        )
+      })}
     </div>
   )
 }
